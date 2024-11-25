f3f43c56e3efe18178dd45b2916b78d44d9f9ab4
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2024-11-25 11:07:04 +0430

    CreateUser componet is added and some styles from tailwindcss

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import CreateUser from '../features/user/CreateUser'
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="mb-4 text-center text-xl font-semibold">
+                The best pizza.
+                <br />
+                <span className="text-yellow-500">
+                    Straight out of the oven, straight to you.
+                </span>
+            </h1>
+            <CreateUser />
+        </div>
+    )
 }
 
-export default Home;
+export default Home
