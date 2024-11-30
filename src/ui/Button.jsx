475c7ce9729dc935b250f616e77f2eda3b3496d2
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2024-11-30 14:42:36 +0430

    OnClick prop is added conditionaly to the Button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,20 +1,31 @@
 import { Link } from 'react-router-dom'
 
 // eslint-disable-next-line react/prop-types
-export default function Button({ children, disabled, to, type }) {
+export default function Button({ children, disabled, to, type, onClick }) {
     const base =
-        'translate-wide inline-block rounded-full bg-yellow-400  font-semibold uppercase text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed'
+        'translate-wide text-sm inline-block rounded-full bg-yellow-400  font-semibold uppercase text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed'
     const styles = {
         primary: base + 'px-4 py-3 md:px-6 md:py-4',
         small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
         secondery:
-            'px-4 py-2.5 md:px-6 md:py-3.5 translate-wide inline-block border-2 border-stone-300 rounded-full   font-semibold uppercase text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed',
+            'px-4 py-2.5 md:px-6 md:py-3.5 text-sm translate-wide inline-block border-2 border-stone-300 rounded-full   font-semibold uppercase text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed',
     }
     if (to) {
         return (
             <Link to={to} className={styles[type]}>
                 {children}
             </Link>
+        )
+    }
+    if (onClick) {
+        return (
+            <button
+                onClick={onClick}
+                disabled={disabled}
+                className={styles[type]}
+            >
+                {children}
+            </button>
         )
     }
     return (
