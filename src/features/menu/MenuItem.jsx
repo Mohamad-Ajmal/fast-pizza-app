405b8d528f8a2e7c637e6eec0d464747d5b1ca4f
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2024-11-30 14:41:52 +0430

    Add pizza  crom menu to cart is implemented

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,10 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useDispatch } from 'react-redux'
 import Button from '../../ui/Button'
 import { formatCurrency } from '../../utils/helpers'
+import { addItem } from '../cart/cartSlice'
 // eslint-disable-next-line react/prop-types
 function MenuItem({ pizza }) {
+    const dispatch = useDispatch()
     // eslint-disable-next-line react/prop-types
-    const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza
+    const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza
+    function handleAddToCart() {
+        const newItem = {
+            pizzaId: id,
+            name,
+            quantity: 1,
+            unitPrice,
+            totalPrice: unitPrice * 1,
+        }
+
+        dispatch(addItem(newItem))
+    }
     return (
         <li className="flex gap-4 py-2">
             <img
@@ -25,7 +39,11 @@ function MenuItem({ pizza }) {
                             Sold out
                         </p>
                     )}
-                    <Button type="small">Add to cart</Button>
+                    {!soldOut && (
+                        <Button type="small" onClick={handleAddToCart}>
+                            Add to cart
+                        </Button>
+                    )}
                 </div>
             </div>
         </li>
